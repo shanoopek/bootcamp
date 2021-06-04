6d7b26145efd9e0f0d3ecf50b4e611ab4948382e
shanoopek <muhammadshanoopek@cet.ac.in>
2021-06-04 20:16:28 +0530

    first commit

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+        height: 100,
+        width: 100,
+        color: Colors.blueAccent,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('data'),
+            Text('data'),
+          ],
+        ),
+      ),
+    ));
+  }
+}
